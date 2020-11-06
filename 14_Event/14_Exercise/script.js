60398ac0eb1ec395764bc85f4c825038e1540d34
umaporn-sup <umaporn@sit.kmutt.ac.th>
2020-11-06 11:59:49 +0700

    [Add] 14 Event Exercise Solution

--- 14_Event/14_Exercise/script.js
+++ 14_Event/14_Exercise/script.js
@@ -71,13 +71,104 @@ let profiles=[
     }
 ];
 
+// set starting item
+let currentItem = 0;
+let prevProfileIndex;
+//select person
+const lect=document.getElementById("lect");
+const imgcontainer = document.getElementById("img-container");
+const academic = document.getElementById("academic");
+const teaching = document.getElementById("teaching");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const surprisedBtn=document.querySelector(".surprised-btn");
 
-//Show active current Person and move previous back to thumbnail
-//Show next person
-//Show previous person
-//Show random person
-//Search staff name
+function showPerson(personIndex){
+    const item=profiles[personIndex];
+    lect.textContent=item.lect;
+    imgcontainer.src=item.img;
+    
+    let oldAcadLiNode = academic.lastElementChild;
+    while (oldAcadLiNode) {
+      academic.removeChild(oldAcadLiNode);
+      oldAcadLiNode = academic.lastElementChild;
+    }
+    for(let i=0;i<item.academic.length;i++){
+      let newAcadLiNode=document.createElement("li");
+      newAcadLiNode.textContent = item.academic[i];
+      academic.appendChild(newAcadLiNode);
+    }
 
+    oldTeacLiNode = teaching.lastElementChild;
+    while (oldTeacLiNode) {
+      teaching.removeChild(oldTeacLiNode);
+      oldTeacLiNode = teaching.lastElementChild;
+    }
+
+    for (let i = 0; i < item.teaching.length; i++) {
+       let newTeacLiNode = document.createElement("li");
+       newTeacLiNode.textContent = item.teaching[i];
+       teaching.appendChild(newTeacLiNode);
+     }
+}
+
+// show next person
+nextBtn.addEventListener("click", function () {
+  prevProfileIndex = currentItem;
+  currentItem++;
+  if (currentItem > profiles.length - 1) {
+    currentItem = 0;
+  }
+  addToThumbnail(prevProfileIndex, currentItem);
+  showPerson(currentItem);
+});
+
+// show prev person
+prevBtn.addEventListener("click", function () {
+  prevProfileIndex = currentItem;
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = profiles.length - 1;
+  }
+  addToThumbnail(prevProfileIndex, currentItem);
+  showPerson(currentItem);
+});
+
+// show random person
+surprisedBtn.addEventListener("click", function () {
+  prevProfileIndex = currentItem;
+  currentItem = Math.floor(Math.random() * profiles.length);
+  addToThumbnail(prevProfileIndex, currentItem);
+  showPerson(currentItem);
+});
+
+function addToThumbnail(oldProfileIndex, currentProfileIndex){
+    let replaceThumbnail = document.getElementById(
+      profiles[currentProfileIndex].id
+    );
+    let replaceLectQueryImg = replaceThumbnail.getElementsByTagName("img")[0];
+    let replaceLectQueryP = replaceThumbnail.getElementsByTagName("p")[0];
+    replaceLectQueryImg.src = profiles[oldProfileIndex].img;
+    replaceLectQueryP.textContent = profiles[oldProfileIndex].lect;
+    replaceThumbnail.id = profiles[oldProfileIndex].id;
+     
+}
+
+//search lecturer name
+let findLect=function (){
+let lectQuery = document.getElementById("lect-value").value;
+for(let i=0;i<profiles.length;i++){
+      if (profiles[i].id.includes(lectQuery.toLowerCase())){
+        prevProfileIndex = currentItem;
+        currentItem = i;
+        addToThumbnail(prevProfileIndex, currentItem);
+        showPerson(currentItem);
+        break;
+      }
+  }
+}
+let searchBtn = document.getElementById("lect-search");
+searchBtn.addEventListener("click", findLect);
 
 function onlyAlpha(event){
   let letterCode = event.keyCode;
